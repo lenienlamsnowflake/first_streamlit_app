062180694e8e1cc1d091f725f4ad26bb7d034d3c
lenienlamsnowflake <123936637+lenienlamsnowflake@users.noreply.github.com>
2023-01-30 20:47:31 +1100

    Update streamlit_app.py
    added more text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title['My parents New Healthy diner']
+streamlit.title('My parents New Healthy diner')
+streamlit.header('breakfast menu')
+streamlit.text('Boiled egg')
+streamlit.text('Omega 3 & Blue berry Oatmeal')
+streamlit.text('Super8 Breakfast Juice')
