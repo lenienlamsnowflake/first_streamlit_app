f3e91776d1cfbc48e68d325f1f169a20b3a8971d
lenienlamsnowflake <123936637+lenienlamsnowflake@users.noreply.github.com>
2023-01-30 20:54:57 +1100

    Update streamlit_app.py
    added graphics in text lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,12 @@ import streamlit
 streamlit.title('My parents New Healthy diner')
 streamlit.header('breakfast menu')
 streamlit.text('Boiled egg')
-streamlit.text('Omega 3 & Blue berry Oatmeal')
+streamlit.text('🥣Multi grain Honey Oatmeal')
 streamlit.text('Super8 Breakfast Juice')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text('🍌Banana')
+streamlit.text('🥭Strawberry')
+streamlit.text('🥝Kiwi')
+streamlit.text('🍇Graphes')
+streamlit.text('🥑Avacadro')
