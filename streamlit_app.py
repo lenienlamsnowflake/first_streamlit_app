e7712d971d931ec177d8c6d433cd3b8a32e03304
lenienlamsnowflake <123936637+lenienlamsnowflake@users.noreply.github.com>
2023-01-31 14:52:48 +1100

    Update streamlit_app.py
    Try to put a pick list here so they can pick the fruit they want to include (with prepopulated avocado & no name fruit)

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,6 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# Let's put a pick list here so they can pick the fruit they want to include (with prepopulated avocado & no name fruit) 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['avocado','no_name fruit'])
 streamlit.dataframe(my_fruit_list)
